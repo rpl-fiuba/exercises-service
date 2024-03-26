58c31ef7330cb69315601f34364dfac06dafa3ed
Lorenzo Gimenez Luzny <lgimenez@fi.uba.ar>
2024-03-25 21:21:30 -0300

    Revert "Add Trigonometry exercise"
    This reverts commit dff9f72f1398b6763a45bda1a434321d33d91fc4.

--- src/controllers/exerciseController.js
+++ src/controllers/exerciseController.js
@@ -3,7 +3,7 @@ const createError = require('http-errors');
 const expressify = require('expressify')();
 const exerciseService = require('../services/exerciseService');
 
-const allowedExerciseTypes = ['derivative', 'integral', 'factorisable', 'domain', 'inequality', 'image', 'trigonometry'];
+const allowedExerciseTypes = ['derivative', 'integral', 'factorisable', 'domain', 'inequality', 'image'];
 
 const extractMetadata = (body) => (
   _.pick(body, ['problemInput', 'name', 'description', 'type', 'difficulty', 'initialHint'])
